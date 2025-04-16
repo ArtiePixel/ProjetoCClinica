552fa648386cd04ecaf4ea58a6f623ddb615aaa5
ArtiePixel <luizarthursjp@gmail.com>
2025-04-15 21:03:24 -0300

    adicionada a funcionalidade das pastas

--- Cadastro.c
+++ Cadastro.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 
 // struct paciente
 typedef struct {
@@ -10,6 +13,8 @@ typedef struct {
 } Paciente;
 
 void cadastrarPacientes(Paciente *pacientes, int quantidade){
+    mkdir("./pacientes", 0777);
+
     for (int i = 0; i < quantidade; i++) {
 
         printf("\nCadastro do Paciente %d:\n", i+1);
@@ -20,8 +25,13 @@ void cadastrarPacientes(Paciente *pacientes, int quantidade){
         printf("Idade: ");
         scanf("%d", &pacientes[i].idade);
 
-        char nomeArquivo[60];
-        snprintf(nomeArquivo, sizeof(nomeArquivo), "%s.txt", pacientes[i].nome);
+        char nomePasta[300];
+        snprintf(nomePasta, sizeof(nomePasta)*2, "./pacientes/%s", pacientes[i].nome);
+        mkdir(nomePasta, 0777);
+
+
+        char nomeArquivo[300];
+        snprintf(nomeArquivo, sizeof(nomeArquivo)*5, "%s/%s.txt", nomePasta, pacientes[i].nome);
 
         FILE *arquivo = fopen(nomeArquivo, "w");
         if (arquivo == NULL) {
@@ -35,7 +45,7 @@ void cadastrarPacientes(Paciente *pacientes, int quantidade){
                 pacientes[i].idade);
 
         fclose(arquivo);
-        printf("Arquivo %s criado com sucesso!\n", nomeArquivo);
+        printf("Arquivo criado com sucesso em %s!\n", nomeArquivo);
     }
 }
 
